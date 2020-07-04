cd6d81ca0da5087237c232fef3d9aa96b61e1be3
Aayog <1aayogkoirala@gmail.com>
2020-07-03 23:19:36 -0400

    Finished adding random 10 articles for not logged in users

--- wikinterests/interest_site/articles.py
+++ wikinterests/interest_site/articles.py
@@ -1,20 +1,35 @@
-import wikipedia as wiki
-from random import randint
+import requests
+from bs4 import BeautifulSoup
+
+def getArticle(title):
+    baseurl = 'https://en.wikipedia.org/api/rest_v1/page/summary/' + title
+    resp = requests.get(baseurl)
+    data = resp.json()
+    return data['extract']
+
+def getSummary(response):
+    html = response.content
+    soup = BeautifulSoup(html, 'html.parser')
+    information = {}
+    information['title'] = soup.find_all("h1", class_="firstHeading")[0].text
+    paras = soup.find("div", class_= 'mw-parser-output')
+    if "empty" not in str(paras.p):
+        if len(paras.p.text) > 2:
+            information['summary'] = paras.p.text
+    information['url'] = response.url
+    return information
+    # mw-body-content mw-content-ltr mw-parser-output
+    
 def getArticles(n):
-    articles = {}
-    count = 0
-    while True:
-        num = randint(0, 100000)
-        try:
-            url = wiki.page(num)
-            articles[url.url] = f'''
-                <h3>{url.title}</h3>
-                <p>{url.summary}</p>
-                <a href='{url.url}'>See more...</a>
-            '''
-            count += 1
-        except:
-            pass
-        if count == n:
-            break
+    articles = []
+    i = 0 
+    while i < n:
+        url = 'https://en.wikipedia.org/wiki/Special:Random'
+        resp = requests.get(url)
+        info = getSummary(resp)
+        if info.get('summary') is None:
+            continue
+        i += 1
+        articles.append(info)
     return articles
+    
--- wikinterests/interest_site/views.py
+++ wikinterests/interest_site/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from .articles import getArticles
+
 def home(request):
+    articles = [getHtml(a) for a in getArticles(10)]
     context = {
         'title' : 'HOME',
-        'articles' : getArticles(10)
+        'articles' : articles
     }
-    print(context['articles'])
     return render(request, 'interest_site/home.html', context)
 
 
@@ -15,3 +16,10 @@ def about(request):
         'content' : 'Wikinterest is a site that has articles based on users interest.'
     }
     return render(request, 'interest_site/about.html', context)
+
+def getHtml(article):
+    return f"""
+        <h3>{article['title']}</h3>
+        <p>{article['summary']}</p>
+        <a href="{article['url']}">See more...</a>
+    """
